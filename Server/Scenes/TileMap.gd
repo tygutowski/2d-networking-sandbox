91f1a72422fd9e1df22a3c26818a7cf34073b746
Ty Gutowski <tygutowski@gmail.com>
2021-09-14 10:24:00 -0400

    Add files via upload

---
+++ Server/Scenes/TileMap.gd
@@ -0,0 +1,34 @@
+#tool
+extends TileMap
+
+export (int) var counter = 0
+export (bool) var generate_autotile_collision = false
+var tile_data = {}
+
+var tile_size = 16
+var sprite_sheet_size = Vector2(16, 16)
+
+func _ready():
+	if Engine.editor_hint:
+		_generate_autotile_collision()
+
+func _generate_autotile_collision():
+	for x in sprite_sheet_size.x:
+		for y in sprite_sheet_size.y:
+			var shape = ConvexPolygonShape2D.new()
+			shape.points = [Vector2(0,0), Vector2(0, tile_size), Vector2(tile_size, tile_size), Vector2(tile_size, 0)]
+			tile_set.tile_add_shape(0, shape, Transform2D(0.0, Vector2(0,0)), false, Vector2(x,y))
+
+func get_tile_id(block_name):
+	return TileData.tile_data[block_name]["tile id"]
+
+
+func get_item_id(block_name):
+	return TileData.tile_data[block_name]["item id"]
+
+
+func get_tile_atlas_coordinates(block_name):
+	var tile_id = int(TileData.tile_data[block_name]["tile id"])
+	var x = tile_id%16
+	var y = int(tile_id/16)
+	return(Vector2(x,y))
